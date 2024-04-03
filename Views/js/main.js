cc052d4a1c63335670101a4cd12951a0a3d7199c
ZEROX <115658302+Z-E-R-O-X@users.noreply.github.com>
2024-04-03 15:44:58 -0500

    Nuevas funciones
    Función manejarInputBuscar para el buscador de del navbar y función mostrarImagenesAleatorias para el inicio

--- Views/js/main.js
+++ Views/js/main.js
@@ -1,22 +1,73 @@
-document.getElementById("bars").addEventListener("click", mostrar_menu);
-document.getElementById("back_menu").addEventListener("click", ocultar_menu);
+//------------------------------------------------------------------------------------//
+function manejarInputBuscar() {
+    // Obtener referencias a los elementos del DOM
+    var inputBuscar = document.getElementById('inputBuscar');
+    var iconoBuscar = document.getElementById('iconoBuscar');
 
+    // Agregar controlador de eventos de clic al ícono de búsqueda
+    iconoBuscar.addEventListener('click', function (event) {
+        // Mostrar el input
+        inputBuscar.style.display = 'block';
+        // Habilitar el input para escribir
+        inputBuscar.disabled = false;
+        // Poner el foco en el input
+        inputBuscar.focus();
+        // Detener la propagación del evento para evitar que se propague al document
+        event.stopPropagation();
+    });
 
-menu = document.getElementById("menu-container");
-background_menu = document.getElementById("back_menu");
-
-function mostrar_menu(){
-    menu.style.right = "0px";
-    background_menu.style.display = "block"
+    // Agregar controlador de eventos de clic al document
+    document.addEventListener('click', function (event) {
+        // Ocultar el input si se hace clic en cualquier parte del documento excepto en el input
+        if (event.target !== inputBuscar && event.target !== iconoBuscar) {
+            inputBuscar.style.display = 'none';
+        }
+    });
 }
+document.addEventListener('DOMContentLoaded', manejarInputBuscar);
+//------------------------------------------------------------------------------------//
 
-function ocultar_menu(){
-    menu.style.right = "-250px";
-    background_menu.style.display = "none"
+var imagenes = [
+    "Views/assets/img/curso algebra.jpeg",
+    "Views/assets/img/curso c++.jpeg",
+    "Views/assets/img/curso calculo.jpeg",
+    "Views/assets/img/curso canva.jpeg",
+    "Views/assets/img/curso coreano.jpeg",
+    "Views/assets/img/curso estres.jpeg",
+    "Views/assets/img/curso excel.jpeg",
+    "Views/assets/img/curso frances.jpeg",
+    "Views/assets/img/curso html.jpeg",
+    "Views/assets/img/curso ingles.jpeg",
+    "Views/assets/img/curso japon.jpeg",
+    "Views/assets/img/curso java.jpeg",
+    "Views/assets/img/curso perdida de audición.jpeg",
+    "Views/assets/img/curso peligros biologicos.jpeg",
+    "Views/assets/img/curso peligros eletricos.jpeg",
+    "Views/assets/img/curso photoshop.jpeg",
+    "Views/assets/img/curso picmonkey.jpeg",
+    "Views/assets/img/curso pixlr.jpeg",
+    "Views/assets/img/curso probabilidad.jpeg",
+    "Views/assets/img/curso python.jpeg",
+    "Views/assets/img/curso sql server.jpeg",
+    "Views/assets/img/curso kotlin.jpeg",
+];
+function obtenerImagenAleatoria() {
+    var indiceAleatorio = Math.floor(Math.random() * imagenes.length);
+    return imagenes.splice(indiceAleatorio, 1)[0];
 }
+function mostrarImagenesAleatorias(contenedorId) {
+    var contenedor = document.getElementById(contenedorId);
+    contenedor.innerHTML = ''; 
 
-
-
-function button_login(ruta) {
-    window.location.href = ruta;
+    for (var index = 0; index < 7; index++) {
+        var imagenElemento = document.createElement('img');
+        imagenElemento.src = "http://localhost/CLAY/" + obtenerImagenAleatoria();
+        contenedor.appendChild(imagenElemento);
+    }
 }
+window.onload = function() {
+    mostrarImagenesAleatorias('imagen-container1');
+    mostrarImagenesAleatorias('imagen-container2');
+    mostrarImagenesAleatorias('imagen-container3');
+};
+//------------------------------------------------------------------------------------//
