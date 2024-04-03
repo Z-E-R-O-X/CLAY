03e4998bb59106248594d4752a260eb04a6289e3
ZEROX <115658302+Z-E-R-O-X@users.noreply.github.com>
2024-04-03 10:29:34 -0500

    Delete CLAY directory

--- CLAY/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once "./Configuration/App.php";
-
-require_once "./Controller/VistaControlador.php";
-
-$plantillas = new VistaControlador();
-
-$plantillas->obtener_plantilla_controlador();
