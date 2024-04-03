0f625aa075d0b14ab0d861dc5c1a7de16e800be9
ZEROX <115658302+Z-E-R-O-X@users.noreply.github.com>
2024-04-03 15:35:12 -0500

    Avance del contenido de la pagina de inicio

---
+++ Views/content/inicio-view.php
@@ -0,0 +1,18 @@
+<div class="container-inicio">
+    <div class="container-inicio-one">
+        <div class="image-container" id="imagen-container1">
+        </div>
+        <div class="image-container" id="imagen-container2">
+        </div>
+        <div class="image-container" id="imagen-container3">
+        </div>
+        <img class="background-image" src="Views/assets/img/fondo.jpg" />
+        <div class="gradient-overlay"></div>
+        <div class="gradient-background"></div>
+    </div>
+</div>
+
+
+
+<script src="<?= SERVERURL ?>/Views/js/main.js"></script>
+
